ea0ab4cf1fb23f22e11a0a53c7cc1d5b396090f9
Akai <huy123nn69hz@gmail.com>
2025-07-02 20:18:03 +0700

    update

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Loading = () => {
-  return null;
+  return (
+    <div className='fixed inset-0 z-[9999] bg-[url("/images/loading/bg.png")] bg-cover bg-center bg-no-repeat'></div>
+  );
 };
 
 export default Loading;
