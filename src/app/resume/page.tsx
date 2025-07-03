5e0244b53494a8956f6a335639ac4549740dddcd
Akai <huy123nn69hz@gmail.com>
2025-07-04 00:38:18 +0700

    update

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResumePage = () => {
+  return (
+    <div id='section' className=''>
+      Resume Page
+    </div>
+  );
+};
+
+export default ResumePage;
---
+++ src/app/works/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WorksPage = () => {
+  return (
+    <div id='section' className=''>
+      Works Page
+    </div>
+  );
+};
+
+export default WorksPage;
