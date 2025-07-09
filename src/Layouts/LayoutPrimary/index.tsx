3bb934a99b963865595e631f6ed16c4beea4f177
Akai <huy123nn69hz@gmail.com>
2025-07-09 11:42:16 +0700

    update

--- src/Layouts/LayoutPrimary/index.tsx
+++ src/Layouts/LayoutPrimary/index.tsx
@@ -46,10 +46,10 @@ const LayoutPrimary = ({ children }: any) => {
         ease: 'power2.inOut',
       });
       // setTimeout(() => {
-      gsap.set('#layout-primary', {
-        overflow: 'hidden',
-        // maxHeight: '100vh',
-      });
+      // gsap.set('#layout-primary', {
+      //   overflow: 'hidden',
+      //   // maxHeight: '100vh',
+      // });
       gsap.to('#layout-primary', {
         // scale: 1,
         // opacity: 1,
@@ -58,10 +58,10 @@ const LayoutPrimary = ({ children }: any) => {
         ease: 'power2.inOut',
         onComplete: () => {
           setIsChangingPage(false);
-          gsap.set('#layout-primary', {
-            overflow: 'visible',
-            maxHeight: 'unset',
-          });
+          // gsap.set('#layout-primary', {
+          //   overflow: 'visible',
+          //   maxHeight: 'unset',
+          // });
         },
       });
       // }, 500);
@@ -89,10 +89,10 @@ const LayoutPrimary = ({ children }: any) => {
         duration: 0.8,
         ease: 'power2.inOut',
       });
-      gsap.set('#layout-primary', {
-        maxHeight: '100vh',
-        overflow: 'hidden',
-      });
+      // gsap.set('#layout-primary', {
+      //   maxHeight: '100vh',
+      //   overflow: 'hidden',
+      // });
       gsap.to('#layout-primary', {
         // scale: 0.9,
         // opacity: 0,
@@ -138,13 +138,13 @@ const LayoutPrimary = ({ children }: any) => {
       )}
       {welcomeState !== PAGE_STATE.LOADING && (
         <>
-          {pathname === '/' && (
-            <div
-              id='bg-video'
-              className='pointer-events-none fixed inset-0 opacity-0'
-            >
-              <div className='absolute inset-0 bg-black bg-opacity-50'></div>
-              <video
+          {/* {pathname === '/' && ( */}
+          <div
+            id='bg-video'
+            className='pointer-events-none fixed inset-0 opacity-0'
+          >
+            <div className='absolute inset-0 z-10 bg-black bg-opacity-50'></div>
+            {/* <video
                 autoPlay
                 loop
                 muted
@@ -156,8 +156,18 @@ const LayoutPrimary = ({ children }: any) => {
                   type='video/mp4'
                   className='object-cover'
                 />
-              </video>
-
+              </video> */}{' '}
+            <div className='relative h-full w-full overflow-hidden'>
+              <iframe
+                className='absolute left-1/2 top-1/2 h-[56.25vw] min-h-full w-full min-w-[177.77vh] -translate-x-1/2 -translate-y-1/2 border-0'
+                src='https://www.youtube-nocookie.com/embed/B4du2GEuTw4?autoplay=1&mute=1&loop=1&playlist=B4du2GEuTw4&controls=0&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&playsinline=1&enablejsapi=1&origin=window.location.origin'
+                title='YouTube video player'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                referrerPolicy='strict-origin-when-cross-origin'
+                allowFullScreen
+              ></iframe>
+            </div>
+            {pathname === '/' && (
               <CustomImage
                 src='/images/home/tqn.webp'
                 className={`fixed right-0 top-0 ${!isShowMenu ? 'opacity-100' : 'opacity-0'} transition-all duration-[1000ms] ease-in-out`}
@@ -165,10 +175,10 @@ const LayoutPrimary = ({ children }: any) => {
                 width={453}
                 height={688}
               />
-            </div>
-          )}
+            )}
+          </div>
 
-          <div id='layout-primary'>
+          <div id='layout-primary' className='relative'>
             <div
               id='layout-primary-pathname'
               className='fixed inset-0 z-[-1] flex items-center justify-center bg-[url("/images/home/bg.png")] bg-cover bg-center bg-no-repeat text-[48px] text-[#F4E4CA] opacity-0'
