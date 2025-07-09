b904338a5244e81205c005dcd5e712456213a876
Akai <huy123nn69hz@gmail.com>
2025-07-09 23:51:27 +0700

    update icon mail scale

--- src/assets/icons/IconMail.tsx
+++ src/assets/icons/IconMail.tsx
@@ -1,10 +1,12 @@
+import { customUnitFn } from '@/utils';
 import * as React from 'react';
 import { SVGProps } from 'react';
 const IconMail = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
-    width={32}
-    height={32}
+    width={customUnitFn(32)}
+    height={customUnitFn(32)}
+    viewBox='0 0 32 32'
     fill='none'
     {...props}
   >
